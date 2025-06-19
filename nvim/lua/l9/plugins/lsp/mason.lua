65bb8b3f2f2a45675cac185889029934144d0952
SINHLONG <l9dev@Mac.lan>
2025-06-19 20:00:10 +0700

    update config

--- nvim/lua/l9/plugins/lsp/mason.lua
+++ nvim/lua/l9/plugins/lsp/mason.lua
@@ -27,23 +27,26 @@ return {
 			},
 		})
 
-		-- mason_lspconfig.setup({
-		-- 	-- list of servers for mason to install
-		-- 	ensure_installed = {
-		-- 		"html",
-		-- 		"cssls",
-		-- 		"tailwindcss",
-		-- 		"svelte",
-		-- 		"lua_ls",
-		-- 		"graphql",
-		-- 		"emmet_ls",
-		-- 		"prismals",
-		-- 		"pyright",
-		-- 	},
-		-- 	-- auto-install configured servers (with lspconfig)
-		-- 	automatic_installation = true, -- not the same as ensure_installed
-		-- 	automatic_enable = true,
-		-- })
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"lua_ls",
+				"gopls",
+				"lua-language-server",
+				"graphql",
+				"emmet_ls",
+				"prismals",
+				"pyright",
+				"ruff",
+			},
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+			automatic_enable = true,
+		})
 
 		mason_tool_installer.setup({
 			ensure_installed = {
